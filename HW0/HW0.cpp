24193b23c2058406afc2f9b3abdf229290ba51a5
TriggerMoke <michalosaidan@gmail.com>
2024-01-17 13:19:50 -0700

    HW0 Init

---
+++ HW0/HW0.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+   // Knock Knock
+   cout << "Knock Knock\n"
+   << "Who's There\n"
+   << "Monica\n" << "Monica who?\n" 
+   << "Harmonica \"Sic Harmonica Solo\"\n";
+   return 0;
+}
