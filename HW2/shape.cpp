78d508df1c2e2d53e5cd270592fb80f1bf7a8bfd
Aidan <61124621+TriggerMoke@users.noreply.github.com>
2024-02-26 14:45:21 -0700

    pain

--- HW2/shape.cpp
+++ HW2/shape.cpp
@@ -1,8 +1,5 @@
 #include "shape.h"
 
-std::ostream &operator<<(std::ostream &os, const Shape *shape) {
-    if (shape) // Ensure the pointer is not null
-        return shape->Print(os); // Call the virtual Print method
-    else
-        return os << "Null Shape\n"; // Handle null pointer case
+std::ostream &operator<<(std::ostream &os, const Shape &shape) {
+    return shape.Print(os);
 }
