a0cd46dfddf82000bf22a5d7942c54c12e10e533
Aidan <61124621+TriggerMoke@users.noreply.github.com>
2024-03-13 13:44:12 -0600

    comment update

--- ASN2/game_of_life.h
+++ ASN2/game_of_life.h
@@ -37,8 +37,8 @@ public:
     int GetGenerations() const;
 
     // Changes the current Live or Dead cell to a new character. 
-    //It cannot be set to whatever the current Dead/Live Cell character is.
-    //It throws a runtime error if the display characters are duplicated
+    // It cannot be set to whatever the current Dead/Live Cell character is.
+    // It throws a runtime error if the display characters are duplicated
     void SetLiveCell(char liveCell);
     void SetDeadCell(char deadCell);
     void UpdateCellChars(char oldChar, char newChar);
